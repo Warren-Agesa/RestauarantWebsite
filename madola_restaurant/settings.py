babecfcb49dc8f384581187a9bfd3a43f706e15e
Warren.Karanja <warrenfred98@gmail.com>
2026-02-19 22:11:17 +0300

    Update project: add static files and latest changes

--- madola_restaurant/settings.py
+++ madola_restaurant/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
+from django.contrib.auth import get_user_model
 
 load_dotenv()
 
@@ -181,3 +182,14 @@ CSRF_TRUSTED_ORIGINS = [
     'https://madola.up.railway.app',
     "https://www.mydomain.com",
 ]
+
+if os.environ.get("RAILWAY_DEPLOY") == "true":
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if username and email and password:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            print(f"Superuser '{username}' created.")

35f64699ce55a4f1ceb0bfdf8c53f2d404a0c9a8
Warren.Karanja <warrenfred98@gmail.com>
2026-02-19 22:23:53 +0300

    Update project: add static files and latest changes

---
+++ restaurant/management/commands/createsu.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = 'Creates a superuser from environment variables'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if username and email and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+                self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' created."))
+            else:
+                self.stdout.write(f"Superuser '{username}' already exists.")
+        else:
+            self.stdout.write("Environment variables for superuser not set.")

4e4387fdfd36940020ebdd4dcfd8fe91c5c10f2d
Warren.Karanja <warrenfred98@gmail.com>
2026-02-19 14:49:50 +0300

    Update project files

--- madola_restaurant/settings.py
+++ madola_restaurant/settings.py
@@ -176,3 +176,8 @@ PASSKEY = os.environ.get("PASSKEY")
 CALLBACK_URL = os.environ.get("CALLBACK_URL")
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-temp-key-for-dev")
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-9c9e7.up.railway.app",
+    "https://www.mydomain.com",
+]
